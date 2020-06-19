24f6e07fae1d08b7fb5e978667a0d5f5b5b7e503
JimmyMcBride <mcbride967@gmail.com>
2020-06-18 22:25:30 -0500

    more fixesgitit more

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import App from "next/app";
-import { AppWrapper, theme } from "sriracha-ui";
+// import { AppWrapper, theme } from "sriracha-ui";
 import "./styles.css";
 import "sriracha-ui/css/main.css";
 
@@ -8,9 +8,9 @@ class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <AppWrapper className="app" bg={theme.colors.gray5}>
-        <Component {...pageProps} />
-      </AppWrapper>
+      // <AppWrapper className="app" bg={theme.colors.gray5}>
+      <Component {...pageProps} />
+      // </AppWrapper>
     );
   }
 }
