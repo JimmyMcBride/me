168d7394f58fb133a658cf84dd81535a9af648d9
JimmyMcBride <mcbride967@gmail.com>
2020-06-21 12:59:50 -0500

    changed background color

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
 import {
-  AppWrapper,
   Wrapper,
   NavBar,
   Box,
@@ -22,61 +21,56 @@ export default function Layout({
 }) {
   const { isCabinet, toggleCabinet } = useCabinet();
   return (
-    <AppWrapper className="app" bg={theme.colors.gray5}>
-      <Wrapper aiStart minH="100%">
-        <Head>
-          <title>{title}</title>
-          <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-        </Head>
-        <header>
-          <NavBar bg={theme.colors.gray0} drape shadow={theme.shadows[3]}>
-            <Flex stretch jcBetween aiCenter h={navHeight}>
-              <Box
-                className="nav-cabinet-menu"
-                m="0 0 0 2rem"
-                pointer
-                onClick={toggleCabinet}
-              >
-                <Menu style={{ fontSize: "3.8rem" }} />
-              </Box>
-              <Box className="nav-link-list">
-                <Flex m="0 0 0 2rem">
-                  <NavLinks />
-                </Flex>
-              </Box>
-              <Flex aiCenter>
-                <Text
-                  as="p"
-                  lf
-                  m="0"
-                  color={theme.colors.gray9}
-                  className="my-name"
-                >
-                  Jimmy McBride
-                </Text>
-                <Box w="2rem" />
-                <Box sqr="4rem" circle m="0 2rem 0 0">
-                  <img src="/jimicon.png" alt="me" />
-                </Box>
+    <Wrapper aiStart minH="100%">
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <NavBar bg={theme.colors.gray0} drape shadow={theme.shadows[3]}>
+          <Flex stretch jcBetween aiCenter h={navHeight}>
+            <Box
+              className="nav-cabinet-menu"
+              m="0 0 0 2rem"
+              pointer
+              onClick={toggleCabinet}
+            >
+              <Menu style={{ fontSize: "3.8rem" }} />
+            </Box>
+            <Box className="nav-link-list">
+              <Flex m="0 0 0 2rem">
+                <NavLinks />
               </Flex>
+            </Box>
+            <Flex aiCenter>
+              <Text
+                as="p"
+                lf
+                m="0"
+                color={theme.colors.gray9}
+                className="my-name"
+              >
+                Jimmy McBride
+              </Text>
+              <Box w="2rem" />
+              <Box sqr="4rem" circle m="0 2rem 0 0">
+                <img src="/jimicon.png" alt="me" />
+              </Box>
             </Flex>
-          </NavBar>
-        </header>
-        <Cabinet active={isCabinet} toggle={toggleCabinet}>
-          <Flex drape h="15rem" jcEvenly>
-            <NavLinks />
           </Flex>
-        </Cabinet>
-        <Flex drape stretch>
-          <Box h={navHeight} />
-          <Box />
-          {children}
+        </NavBar>
+      </header>
+      <Cabinet active={isCabinet} toggle={toggleCabinet}>
+        <Flex drape h="15rem" jcEvenly>
+          <NavLinks />
         </Flex>
-      </Wrapper>
-    </AppWrapper>
+      </Cabinet>
+      <Flex drape stretch>
+        <Box h={navHeight} />
+        <Box />
+        {children}
+      </Flex>
+    </Wrapper>
   );
 }
